7b228155f7f0a628a852226bb13a9eea7a2ca6d7
dougm1963 <dougmeador1963@gmail.com>
2017-12-16 21:56:53 -0500

    game working, need add for missed letters

--- hangman.rb
+++ hangman.rb
@@ -4,31 +4,29 @@
 	puts "enter your word to be guessed"
 	word_entered = gets.chomp
 
-	word_array = word_entered.split
+	word_array = word_entered.split("")
 	#print word_array
 
+    lives = 10 
+    alphabet_array = ["a", "b", "c", "d", "e", "f", "g", 
+    "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
+    "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    while word_array.count > 0 do 
+    	 
     puts "Guess a letter: "
     print " "
-    guess = gets.chomp.downcase
+    guess = [gets.chomp]
+    # print guess
 
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
-    "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+    # print alphabet_array
+    word_array = word_array - guess 
+    print word_array
+    updated_alphabet_array = alphabet_array - guess
+    print updated_alphabet_array, "\n"
    
-def guess(letters_remaining)
-    letters_remaining = alphabet - guess
-   
-    word_array.delete(word_array.length-1)
-    letters_remaining.delete(letters_remaining.length-1)
-    print letters_remaining
-    
 end
 
-    turn = 0
-    lives = 10 
-  
-   
 
-
-    
-
- 
